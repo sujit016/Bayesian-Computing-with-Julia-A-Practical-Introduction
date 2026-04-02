eceec0af99107d2ab8bc1531d6de5c8990b12f92
Sujit Sandipan Chaugule <135223551+sujit016@users.noreply.github.com>
2026-04-02 10:31:24 +0530

    Update Bayesian Estimation for Linear Regression Problem.jl

--- Julia Files/Bayesian Estimation for Linear Regression Problem.jl
+++ Julia Files/Bayesian Estimation for Linear Regression Problem.jl
@@ -64,7 +64,6 @@ for i in 2:iteration
     end
     prob = tmp_post_β₀ ./ sum(tmp_post_β₀)
     post_β₀[i] = sample(grid_β₀, Weights(prob))
-
     tmp_post_β₁ = zeros(length(grid_β₁))
     for j in eachindex(grid_β₁)
         params = [post_β₀[i], grid_β₁[j], post_ϕ[i-1]]
@@ -72,7 +71,6 @@ for i in 2:iteration
     end
     prob = tmp_post_β₁ ./ sum(tmp_post_β₁)
     post_β₁[i] = sample(grid_β₁, Weights(prob))
-
     shape = α + nrow(data)/2
     rate = (1/2) * sum((y .- post_β₀[i] .- post_β₁[i] .* x).^2) + γ
     post_ϕ[i] = rand(InverseGamma(shape, rate))
@@ -84,51 +82,40 @@ p2 = plot(post_β₁, color = "red", lw = 2, xlabel = "index", ylabel = L"β_1",
     title = "Trace Plot of β₁", label = "")
 p3 = plot(post_ϕ, color = "red", lw = 2, xlabel = "index", ylabel = L"ϕ", 
     title = "Trace Plot of ϕ", label = "")
-
 plot(p1, p2, p3, layout = (2, 2), size = (800,600))
-
-# # Plot of conditional posterior density functions of β₀, β₁, ϕ
+## Plot of conditional posterior density functions of β₀, β₁, ϕ
 cut = 0.7
 burn_in = ceil(Int, iteration * cut)
-
 u = post_β₀[burn_in:end]   
 index = 1:16:length(u)
 post_β₀_thining = u[index]
 acf(post_β₀_thining, xlabel = "lag", ylabel = "ACF" , label = "", 
     title = "acf of posterior β₀")
-
 u = post_β₁[burn_in:end]
 index = 1:16:length(u)
 post_β₁_thining = u[index]
 acf(post_β₁_thining, xlabel = "lag", ylabel = "ACF" , label = "", 
         title = "acf of posterior β₁")
-
 u = post_ϕ[burn_in:end]
 index = 1:16:length(u)
 post_ϕ_thining = u[index]
 acf(post_ϕ_thining, seriestype = :stem, xlabel = "lag", ylabel = "ACF" , label = "", 
         title = "acf of posterior ϕ")
-
 p1 = histogram(post_β₀_thining, normalize = true, xlabel = L"β_0", ylabel = "Density", 
     title = "Posterior of β₀", label = "")
 CI_β₀ = quantile(post_β₀_thining, [2.5, 97.5] ./100)
 vline!([CI_β₀], color = "red", lw = 2 ,label = "95% CI")
-
 p2 = histogram(post_β₁_thining, normalize = true, xlabel = L"β_1", ylabel = "Density", 
     title = "Posterior of β₁", label = "")
 CI_β₁ = quantile(post_β₁_thining, [2.5, 97.5] ./100)
 vline!([CI_β₁], color = "red", lw = 2 ,label = "95% CI")
-
 p3 = histogram(post_ϕ_thining, normalize = true, xlabel = L"ϕ", ylabel = "Density", 
     title = "Posterior of ϕ", label = "")
 CI_ϕ = quantile(post_ϕ_thining, [2.5, 97.5] ./100)
 vline!([CI_ϕ], color = "red", lw = 2 ,label = "95% CI")
-
 mean_post_β₀ = mean(post_β₀_thining)
 mean_post_β₁ = mean(post_β₁_thining)
 shape = α + nrow(data)/2
-
 rate = (1/2)*sum((y .- mean_post_β₀ .- mean_post_β₁ .*x) .^2) + γ
 plot!(x->pdf(InverseGamma(shape, rate), x), color = "magenta", lw = 2, label = "true curve")
 plot(p1, p2, p3, layout = (2, 2), size = (800,600))
-
